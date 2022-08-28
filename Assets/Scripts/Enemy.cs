6a1fe02453efee62e3ec6d00bbbc69dcd6c56369
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-08-28 15:32:58 +0300

    Fix bugs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,22 @@
+using Redcode.Pools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+    public class Enemy : MonoBehaviour, IPoolObject
     {
-        
+        public enum State { Alive, Died }
+        public int Health { get; private set; }
+
+        private State _state;
+
+        // Called when getting this object from pool.
+        public void OnGettingFromPool()
+        {
+            _state = State.Alive;
+            Health = 100;
+
+            print(_state);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void takeDamage(int damage)
-    {
-
-    }
-}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,10 +6,25 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField]
+    private PoolManager _poolManager;
+
+    [SerializeField]
     private Enemy _enemyPrefab;
 
     private void Start()
     {
-        var pool = Pool.Create(_enemyPrefab);
+        // Getting pool from pool manager.
+        // Also has other overloads.
+        var enemyPool = _poolManager.GetPool<Enemy>();
+
+        // Find pool of type Enemy and get object from it.
+        // Also has other overloads.
+        var enemy = _poolManager.GetFromPool<Enemy>();
+
+        // Find pool of type Enemy and returns clone to it.
+        // Also has other overloads.
+        _poolManager.TakeToPool<Enemy>(enemy);
+
+        enemyPool.Get();
     }
 }
